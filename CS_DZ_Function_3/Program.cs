17a85f38a33d09e9d8c9b76583c461b12381bc58
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-04 09:27:53 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_Function_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_Function_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool canExit = false;
+
+            while (!canExit)
+            {
+                Console.WriteLine("Для конвертации введите 1 ");
+                Console.WriteLine("Для выхода введите 2 или exit ");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "1":
+                        Console.WriteLine("Введите число которое нужно сконвертировать: ");
+                        Convert(out int convertedSymbol);
+                        break;
+                    case "2":
+                    case "exit":
+                        canExit = true;
+                        break;
+                }
+                
+            }
+        }
+
+        static void Convert(out int number)
+        {
+            string userInput = Console.ReadLine();
+
+            bool canConvert = int.TryParse(userInput, out number);
+
+            if (canConvert)
+            {
+                Console.WriteLine("Число: " + userInput + " удалось сконвертировать в: " + number);
+            }
+                
+            else
+            {
+                Console.WriteLine("Ошибка конвертации.");
+            }
+        }
+    }
+}
